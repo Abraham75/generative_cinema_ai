727845489b33f76958737cec82987d52f2295952
Abraham75 <44035311+Abraham75@users.noreply.github.com>
2025-09-18 03:33:39 -0400

    Create frame_selector.py

---
+++ frame_selector.py
@@ -0,0 +1,5 @@
+"""
+frame_selector.py - Selects optimal cinematic frame based on narrative semantics
+"""
+def select_frame(script_text):
+    return "Wide aerial shot showcasing temple cliffs at sunset"
