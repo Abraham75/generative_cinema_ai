93eb9ab375656cef3a07a2d6cffb128bb95c7762
Abraham75 <44035311+Abraham75@users.noreply.github.com>
2025-09-18 03:35:09 -0400

    Create texture_generator.py

---
+++ texture_generator.py
@@ -0,0 +1,5 @@
+"""
+texture_generator.py - Applies visual texture layers to enhance mood
+"""
+def generate_visual_texture(script_text):
+    return "Add volumetric light rays and floating dust particles for mysticism"
