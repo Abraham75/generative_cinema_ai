24c806c4e98aec2e890fdd6014da2bdebefa34fa
Abraham75 <44035311+Abraham75@users.noreply.github.com>
2025-09-18 03:34:48 -0400

    Create camera_movement.py

---
+++ camera_movement.py
@@ -0,0 +1,5 @@
+"""
+camera_movement.py - Determines appropriate camera motion based on narrative tension and beat
+"""
+def suggest_camera_movement(script_text):
+    return "Slow dolly-in toward the silhouetted figure to build tension"
